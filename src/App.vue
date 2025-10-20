696d00ecff06495e86f60d1c58b74c66d775e501
Elisei Morosan <elisei.morosan@emanuel.ro>
2025-10-20 20:21:11 +0300

    Finished homework

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+  const colors = ['bg-blue500', 'bg-green-300', 'bg-pink-200', 'bg-gray-400', 'bg-purple-200', 'bg-yellow-500']
+  const colorClass = ref('bg-red-500');
+
+  const changeColor = () => {
+    const randomColor = Math.floor(Math.random() * colors.length);
+    colorClass.value = colors[randomColor];
+  }
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex justify-center items-center flex-col gap-5 h-screen w-screen">
+    <button @click="changeColor" class= "text-[15px] font-semibold px-[40px] py-[8px] bg-gray-200">Genereaza culoare</button>
+    <div :class="['w-[300px]', 'h-[300px]', colorClass, 'border-[2px]']"></div>
+  </div>
 </template>
 
 <style scoped></style>
